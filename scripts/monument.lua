b88ca8c7feb5c14badd7f54f288bbd16fa3aa129
Anton Menzinsky <anton.menzinsky@asurgent.se>
2026-06-14 23:38:44 +0200

    Bugfix incorrect casing on building parts

--- scripts/monument.lua
+++ scripts/monument.lua
@@ -6,55 +6,55 @@ mod:dofile("scripts/building_parts.lua")
 -- Register the monument building
 mod:registerAsset({
     DataType = "BUILDING",
-    Id = "MONUMENT",
-    Name = "MONUMENT_NAME",
-    Description = "MONUMENT_DESC",
+    Id = "LOD_MONUMENT",
+    Name = "LOD_MONUMENT_NAME",
+    Description = "LOD_MONUMENT_DESC",
     BuildingType = BUILDING_TYPE.ADMINISTRATION,
     AssetCoreBuildingPart = "BUILDING_PART_MONUMENT_POLE",
     AssetBuildingPartList = {
         -- Single LOD_X without suffix names
-        "Experiment_10_PART",
-        "Experiment_11_PART",
-        "Experiment_12_PART",
-        "Experiment_13_PART",
-        "Experiment_14_PART",
-        "Experiment_15_PART",
-        "Experiment_16_PART",
-        "Experiment_17_PART",
-        "Experiment_18_PART",
+        "EXPERIMENT_10_PART",
+        "EXPERIMENT_11_PART",
+        "EXPERIMENT_12_PART",
+        "EXPERIMENT_13_PART",
+        "EXPERIMENT_14_PART",
+        "EXPERIMENT_15_PART",
+        "EXPERIMENT_16_PART",
+        "EXPERIMENT_17_PART",
+        "EXPERIMENT_18_PART",
 
-        -- Multiple LOD_X without suffix names
-        "Experiment_20_PART",
-        "Experiment_21_PART",
-        "Experiment_22_PART",
-        "Experiment_23_PART",
-        "Experiment_24_PART",
-        "Experiment_25_PART",
-        "Experiment_26_PART",
-        "Experiment_27_PART",
-        "Experiment_28_PART",
+        -- MULTIPLE LOD_X WITHOUT SUFFIX NAMES
+        "EXPERIMENT_20_PART",
+        "EXPERIMENT_21_PART",
+        "EXPERIMENT_22_PART",
+        "EXPERIMENT_23_PART",
+        "EXPERIMENT_24_PART",
+        "EXPERIMENT_25_PART",
+        "EXPERIMENT_26_PART",
+        "EXPERIMENT_27_PART",
+        "EXPERIMENT_28_PART",
 
-        -- Single LOD_X with suffix names
-        "Experiment_30_PART",
-        "Experiment_31_PART",
-        "Experiment_32_PART",
-        "Experiment_33_PART",
-        "Experiment_34_PART",
-        "Experiment_35_PART",
-        "Experiment_36_PART",
-        "Experiment_37_PART",
-        "Experiment_38_PART",
+        -- SINGLE LOD_X WITH SUFFIX NAMES
+        "EXPERIMENT_30_PART",
+        "EXPERIMENT_31_PART",
+        "EXPERIMENT_32_PART",
+        "EXPERIMENT_33_PART",
+        "EXPERIMENT_34_PART",
+        "EXPERIMENT_35_PART",
+        "EXPERIMENT_36_PART",
+        "EXPERIMENT_37_PART",
+        "EXPERIMENT_38_PART",
 
-        -- Multiple  LOD_X with suffix names
-        "Experiment_40_PART",
-        "Experiment_41_PART",
-        "Experiment_42_PART",
-        "Experiment_43_PART",
-        "Experiment_44_PART",
-        "Experiment_45_PART",
-        "Experiment_46_PART",
-        "Experiment_47_PART",
-        "Experiment_48_PART"
+        -- MULTIPLE  LOD_X WITH SUFFIX NAMES
+        "EXPERIMENT_40_PART",
+        "EXPERIMENT_41_PART",
+        "EXPERIMENT_42_PART",
+        "EXPERIMENT_43_PART",
+        "EXPERIMENT_44_PART",
+        "EXPERIMENT_45_PART",
+        "EXPERIMENT_46_PART",
+        "EXPERIMENT_47_PART",
+        "EXPERIMENT_48_PART"
     }
 })
 
@@ -74,7 +74,7 @@ mod:registerAsset({
             DataType = "GAME_ACTION_UNLOCK_BUILDING_LIST",
             BuildingProgressData = {
                 AssetBuildingList = {
-                    "MONUMENT"
+                    "LOD_MONUMENT"
                 }
             }
         }
